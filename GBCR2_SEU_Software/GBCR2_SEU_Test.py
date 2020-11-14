83bff16f109c482e7089c208e7248250ea67fb44
weizhangccnu <wzhang@mails.ccnu.edu.cn>
2020-11-14 16:14:32 -0600

    iic communication correct

--- GBCR2_SEU_Software/GBCR2_SEU_Test.py
+++ GBCR2_SEU_Software/GBCR2_SEU_Test.py
@@ -7,108 +7,75 @@ import visa
 import datetime
 import struct
 import socket
-from usb_iss import UsbIss, defs
 from GBCR2_Reg import *
+from command_interpret import *
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 '''
 @author: Wei Zhang
-@date: 2020-06-03
-This script is used for testing GBCR2 chip.
+@date: 2020-11-14
+This script is used to test GBCR2 SEU. It mainly includes I2C communication, Ethernet communication, and eight channels bit error record.
 '''
+hostname = '192.168.2.3'                # FPGA IP address
+port = 1024                             # port number
 #------------------------------------------------------------------------------------------------#
-def capture_screen_image(filename):
-    rm = visa.ResourceManager()
-    print(rm.list_resources())
-    inst = rm.open_resource('GPIB2::7::INSTR')              # connect to SOC
-    # print(inst.query("*IDN?"))                              # Instrument ID
-    # time.sleep(0.5)
+## IIC write slave device
+# @param mode[1:0] : '0'is 1 bytes read or wirte, '1' is 2 bytes read or write, '2' is 3 bytes read or write
+# @param slave[7:0] : slave device address
+# @param wr: 1-bit '0' is write, '1' is read
+# @param reg_addr[7:0] : register address
+# @param data[7:0] : 8-bit write data
+def iic_write(mode, slave_addr, wr, reg_addr, data):
+    val = mode << 24 | slave_addr << 17 | wr << 16 | reg_addr << 8 | data
+    cmd_interpret.write_config_reg(4, 0xffff & val)
+    cmd_interpret.write_config_reg(5, 0xffff & (val>>16))
+    time.sleep(0.01)
+    cmd_interpret.write_pulse_reg(0x0001)           # reset ddr3 data fifo
+    time.sleep(0.01)
+#---------------------------------------------------------------------------------------------#
+## IIC read slave device
+# @param mode[1:0] : '0'is 1 bytes read or wirte, '1' is 2 bytes read or write, '2' is 3 bytes read or write
+# @param slave[6:0]: slave device address
+# @param wr: 1-bit '0' is write, '1' is read
+# @param reg_addr[7:0] : register address
+def iic_read(mode, slave_addr, wr, reg_addr):
+    val = mode << 24 | slave_addr << 17 |  0 << 16 | reg_addr << 8 | 0x00     # write device addr and reg addr
+    cmd_interpret.write_config_reg(4, 0xffff & val)
+    cmd_interpret.write_config_reg(5, 0xffff & (val>>16))
+    time.sleep(0.01)
+    cmd_interpret.write_pulse_reg(0x0001)                                     # Sent a pulse to IIC module
 
-    inst.write("*RST")                                      # reset OSC
-    time.sleep(3)
-
-    inst.write(":CHANnel1:DIFFerential ON")                 # Channel 1 and channel 3 differential mode
-
-    inst.write(":AUToscale:VERTical CHAN1")
-    time.sleep(1)
-
-    inst.write(":TIMebase:RANGe 2E-9")
-    time.sleep(1)
-
-    inst.write(":TRIGger:MODE EDGE")                        # set trigger mode
-    # print(inst.query(":TRIGger:MODE?"))
-    inst.write(":TRIGger:EDGE:SOURce CHANnel2")             # channel2 is set to Edge trigger source
-
-    inst.write(":DISPlay:PERSistence INFinite")             # Display Persistence Infinite
-
-    # print(inst.query(":HISTogram:MODE?"))
-    # time.sleep(1)
-    inst.write(":HISTogram:MODE WAVeform")                  # Histogram mode waveform
-    time.sleep(0.5)
-    inst.write(":HISTogram:AXIS HORizontal")                # Histogram horizontal
-    time.sleep(0.5)
-    inst.write(":HISTogram:WINDow:SOURce CHANnel1")         # windown source Channel1
-    time.sleep(0.5)
-    inst.write(":HISTogram:WINDow:LLIMit -600E-12")         #
-    time.sleep(0.5)
-    inst.write(":HISTogram:WINDow:RLIMit 200E-12")
-    time.sleep(1)
-    inst.write(":HISTogram:WINDow:BLIMit -1E-3")
-    time.sleep(0.5)
-    inst.write(":HISTogram:WINDow:TLIMit 1E-3")
-    time.sleep(50)
-
-
-    Hist_Std_Dev = inst.query(":MEASure:HISTogram:STDDev? HISTogram")
-    time.sleep(0.5)
-
-    inst.write(':DISK:CDIRectory "D:\GBCR2_Scan_20200603"')         # set screen image store directory
-    # inst.write(':DISK:MDIRectory "D:\GBCR2_Scan_20200604"')       # make a directory
-    # print(inst.query(":DISK:PWD?"))                                 # current direcotry
-    time.sleep(0.5)
-    inst.write(':DISK:SAVE:IMAGe "%s",BMP,SCReen'%filename)         # save screen image
-    return Hist_Std_Dev
+    val = mode << 24 | slave_addr << 17 | wr << 16 | reg_addr << 8 | 0x00     # write device addr and read one byte
+    cmd_interpret.write_config_reg(4, 0xffff & val)
+    cmd_interpret.write_config_reg(5, 0xffff & (val>>16))
+    time.sleep(0.01)
+    cmd_interpret.write_pulse_reg(0x0001)                                     # Sent a pulse to IIC module
+    time.sleep(0.01)                                                          # delay 10ns then to read data
+    return cmd_interpret.read_status_reg(0) & 0xff
 #---------------------------------------------------------------------------------------------#
 def main():
-    iss = UsbIss()
-    iss.open("COM3")
-    iss.setup_i2c(clock_khz=100, use_i2c_hardware=True, io1_type=None, io2_type=None)
-    I2C_Addr = 0X23
-    Chip_ID = 1
-    CH_ID = 5
-    GBCR2_Reg1 = GBCR2_Reg()
-    CH5_Dis_Rx = 0
-    CH5_CML_AmplSel = 5
-    CH5_Dis_EQ_LF = 0
-    CH5_EQ_ATT = 3
-    CH5_CTLE_HFSR = 11
-    CH5_CTLE_MFSR = 11
-    CH5_Dis_DFF = 1
-    CH5_Dis_LPF = 0
-    with open("Hist_Std_Dev.dat", 'w') as infile:
-        for i in range(0,16):
-            for j in range(0,16):
-                CH5_CTLE_HFSR = i
-                CH5_CTLE_MFSR = j
-                GBCR2_Reg1.set_CH5_CML_AmplSel(CH5_CML_AmplSel)
-                GBCR2_Reg1.set_CH5_CTLE_MFSR(CH5_CTLE_MFSR)
-                GBCR2_Reg1.set_CH5_CTLE_HFSR(CH5_CTLE_HFSR)
-                filename = "Chip=%1d_CH%1d_Dis_Rx=0_CML_AmplSel=%1d_Dis_EQ_LF=0_EQ_ATT=3_CTLE_HFSR=%02d_CTLE_MFSR=%02d_Dis_DFF=1_Dis_LPF=0"%(Chip_ID,CH_ID,CH5_CML_AmplSel,CH5_CTLE_HFSR,CH5_CTLE_MFSR)
-                print(filename)
-                Reg_Write_val = GBCR2_Reg1.get_config_vector()
-                print(Reg_Write_val)
-                iss.i2c.write(I2C_Addr, 0, Reg_Write_val)
-                Reg_Read_val = []
-                Reg_Read_val = iss.i2c.read(I2C_Addr, 0, 0x20)
-                print("GBCR2 I2C Read Back data:")
-                print(Reg_Read_val)
-                print("\n")
-                Hist_Std_Dev = capture_screen_image(filename)
-                print(Hist_Std_Dev)
-                infile.write("%s %.14f\n"%(filename, float(Hist_Std_Dev)))
+    Slave_Addr = 0x23 
+    iic_write_val = GBCR2_Reg1.get_config_vector()
+    print(iic_write_val)
+    
+    ## write data into I2C register one by one
+    for i in range(len(iic_write_val)):
+        iic_write(1, Slave_Addr, 0, i, iic_write_val[i])
+
+    iic_read_val = []
+    ## read back  data from I2C register one by one
+    for i in range(len(iic_write_val)):
+        iic_read_val += [iic_read(0, Slave_Addr, 1, i)]
+    print(iic_read_val)
+
     print("Ok!")
 #------------------------------------------------------------------------------------------------#
 if __name__ == '__main__':
-    main()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #initial socket
+    s.connect((hostname, port))                             #connect socket
+    cmd_interpret = command_interpret(s)                    #Class instance
+    GBCR2_Reg1 = GBCR2_Reg()                                # New a class
+    main()                                                  #execute main function
+    s.close()                                               #close socket
